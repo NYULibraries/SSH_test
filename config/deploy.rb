acedc4ef7678723d45a8996bdbfadb37034ec621
hab278 <hab278@nyu.edu>
2013-11-07 12:29:06 -0500

    Git INIT

---
+++ config/deploy.rb
@@ -0,0 +1,7 @@
+require 'nyulibraries/deploy/capistrano/default_attributes'
+require 'nyulibraries/deploy/capistrano/multistage'
+require 'nyulibraries/deploy/capistrano/rails_config'
+require 'nyulibraries/deploy/capistrano/bundler'
+require 'nyulibraries/deploy/capistrano/rvm'
+set :application, "SSH_test"
+set :repository,  "git@github.com:hab278/SSH_test.git"
