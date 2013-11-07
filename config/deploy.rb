c9244821065f9ff52c893d59efdd15f9ae4b3ce4
hab278 <hab278@nyu.edu>
2013-11-07 13:25:04 -0500

    Ruby and JRuby support, keep 1 release

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,3 +5,4 @@ require 'nyulibraries/deploy/capistrano/bundler'
 require 'nyulibraries/deploy/capistrano/rvm'
 set :application, "SSH_test"
 set :repository,  "git@github.com:hab278/SSH_test.git"
+set :keep_releases, 1
