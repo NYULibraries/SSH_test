ddcce3bc4c9832170f75ba601e98a6b41dd2cc4b
hab278 <hab278@nyu.edu>
2013-11-15 16:42:42 -0500

    Using cap 3

--- old_cap/deploy.rb
+++ old_cap/deploy.rb
@@ -1,29 +1,29 @@
 require 'nyulibraries/deploy/capistrano/default_attributes'
+require 'nyulibraries/deploy/capistrano/multistage'
 require 'nyulibraries/deploy/capistrano/rails_config'
 require 'nyulibraries/deploy/capistrano/bundler'
 require 'nyulibraries/deploy/capistrano/rvm'
 
-set :application, 'SSH_test'
-set :repo_url, 'git@github.com:hab278/SSH_test.git'
+set :application, "SSH_test"
 set :repository,  "git@github.com:hab278/SSH_test.git"
 set :keep_releases, 1
 set :branch, "master"
 
-# namespace :rails_config do
-#   task :set_variables do
+namespace :rails_config do
+  task :set_variables do
     set :app_settings, {
       :user => ENV["USER"],
       :path => ENV["DEPLOY_PATH"],
       :scm_username => ENV["SCM_USERNAME"],
       :servers => [ENV["SERVER"]]
     }
-    set(:scm_username,  ->  { fetch(:app_settings)[:scm_username] } )
-    set(:app_path,      ->  { fetch(:app_settings)[:path] } )
-    set(:user,          ->  { fetch(:app_settings)[:user] } )
-    set(:puma_ports,    ->  { fetch(:app_settings)[:puma_ports] } )
-    set(:deploy_to,     ->  {"#{fetch :app_path}#{fetch :application}"} )
-#   end
-# end
+    set(:scm_username)  { app_settings[:scm_username] }
+    set(:app_path)      { app_settings[:path] }
+    set(:user)          { app_settings[:user] }
+    set(:puma_ports)    { app_settings[:puma_ports] }
+    set(:deploy_to)     {"#{fetch :app_path}#{fetch :application}"}
+  end
+end
 
 namespace :deploy do
   task :migrate do
