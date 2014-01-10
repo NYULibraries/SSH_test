c6352fe5a47261cd2c0dd253b433ded8016caede
hab278 <hab278@nyu.edu>
2014-01-10 15:43:56 -0500

    Removing more fluff

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,30 +1,19 @@
 set :app_title, 'SSH_test'
 set :repo_url, 'git@github.com:hab278/SSH_test.git'
-set :repository,  "git@github.com:hab278/SSH_test.git"
 set :keep_releases, 1
 set :branch, "master"
-set :recipient, "hab278@nyu.edu"
-set :tagging_environments, ["staging", "production"]
 set :log_level, :debug
 set :rvm_custom_path, "~/.rvm"
 set :rvm_ruby_version , "1.9.3"
 
-set :check_app, ENV["CHECK_APP"]
-set :app_url, ENV["APP_URL"]
-set :app_settings, {
-  :user => ENV["USER"],
-  :path => ENV["DEPLOY_PATH"],
-  :scm_username => ENV["SCM_USERNAME"],
-  :servers => [ENV["SERVER"]]
-}
-
-set(:scm_username,  ->  { fetch(:app_settings)[:scm_username] } )
-set(:app_path,      ->  { fetch(:app_settings)[:path] } )
-set(:user,          ->  { fetch(:app_settings)[:user] } )
-set(:puma_ports,    ->  { fetch(:app_settings)[:puma_ports] } )
+set :check_app,     ENV["CHECK_APP"]
+set :app_url,       ENV["APP_URL"]
+set :scm_username,  ENV["SCM_USERNAME"]
+set :app_path,      ENV["DEPLOY_PATH"]
+set :user,          ENV["USER"]
 set(:deploy_to,     ->  {"#{fetch :app_path}#{fetch :application}"} )
 
-fetch(:app_settings)[:servers].each do |srvr| 
+[ENV["SERVER"]].each do |srvr| 
   server srvr, user: fetch(:user), roles: %w{all}
 end
 
