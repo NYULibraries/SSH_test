c36713b72375a9adaf9c2b3fc180eb92a92fdad7
hab278 <hab278@nyu.edu>
2014-01-22 23:33:11 -0500

    legacy name checking

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,7 @@ set :rvm_custom_path,                 ENV.fetch("RVM_PATH", "~/.rvm")
 set :rvm_ruby_version,                ENV.fetch("RUBY_VER", "1.9.3")
 
 set :check_app,     ENV.fetch("CHECK_APP", "")
+set :legacy,        ENV.fetch("LEGACY", false)
 set :app_url,       ENV.fetch("APP_URL", "")
 set :scm_username,  ENV.fetch("SCM_USERNAME", "")
 set :app_path,      ENV.fetch("DEPLOY_PATH", "")
