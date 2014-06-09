1c7522510f6b34c8ecd61b2eded217840aa6b8e1
hab278 <hab278@nyu.edu>
2014-06-09 15:49:28 -0400

    Remove servers for on the fly server config.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,4 @@
+require 'hashie'
 set :app_title, 'SSH_test'
 set :repo_url, 'git@github.com:hab278/SSH_test.git'
 set :keep_releases, 1
@@ -8,14 +9,10 @@ SSHKit.config.command_map[:apache]  = ENV.fetch("APACHE", "httpd")
 set :rvm_custom_path,                 ENV.fetch("RVM_PATH", "~/.rvm")
 set :rvm_ruby_version,                ENV.fetch("RUBY_VER", "1.9.3")
 
-set :check_app,     ENV.fetch("CHECK_APP", "")
+set :check_app,     ENV.fetch("CHECK_APP", "a")
 set :legacy,        ENV.fetch("LEGACY", false)
-set :app_url,       ENV.fetch("APP_URL", "")
-set :scm_username,  ENV.fetch("SCM_USERNAME", "")
-set :app_path,      ENV.fetch("DEPLOY_PATH", "")
-set :user,          ENV.fetch("USER", "")
+set :app_url,       ENV.fetch("APP_URL", "a")
+set :scm_username,  ENV.fetch("SCM_USERNAME", "Jenkins-NYULib")
+set :app_path,      ENV.fetch("DEPLOY_PATH", "/apps/")
+set :user,          "wsops"
 set(:deploy_to,     ->  {"#{fetch :app_path}#{fetch :application}"} )
-
-[ENV.fetch("SERVER", "")].each do |srvr| 
-  server srvr, user: fetch(:user), roles: %w{all}
-end
